719841afacbc81953e655850b9cce29949ecbba5
lopakhatoi <lopakhatoi@gmail.com>
2024-07-17 21:52:43 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoLists from "./components/TodoLists";
+import './App.css';
+
+const App=()=> {
+
+  const initialState=JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput]=useState("");
+  const[todos, setTodos]=useState(initialState);
+  const [editTodo,setEditTodo]=useState(null);
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos]);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form 
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+          />
+         <div>
+          <TodoLists todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+         </div>
+        </div>
+      </div>
+      
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoLists.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const TodoLists = ({ todos, setTodos, setEditTodo }) => {
+  const handleComplete = (todo) => {
+    setTodos(
+      todos.map((item) =>
+        item.id === todo.id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  };
+
+  const handleEdit = ({ id }) => {
+    const findTodo = todos.find((todo) => todo.id === id);
+    setEditTodo(findTodo);
+  };
+
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <ul style={{ listStyle: 'none', padding: 0 }}>
+      {todos.map((todo) => (
+        <li className='list-item' key={todo.id}>
+          <input
+            type='text'
+            value={todo.title}
+            className={`list ${todo.completed ? 'complete' : ''}`}
+            onChange={(event) => event.preventDefault()}
+          />
+          <div>
+            <button
+              className='button-complete task-button'
+              onClick={() => handleComplete(todo)}
+            >
+              <i className='fa fa-check-circle'></i>
+            </button>
+            <button
+              className='button-edit task-button'
+              onClick={() => handleEdit(todo)}
+            >
+              <i className='fa fa-edit'></i>
+            </button>
+            <button
+              className='button-delete task-button'
+              onClick={() => handleDelete(todo)}
+            >
+              <i className='fa fa-trash'></i>
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoLists;
+
